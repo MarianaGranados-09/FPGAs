d2769eecefc40e23285aed7940576a35364519a7
Mariana Granados <89766019+MarianaGranados-09@users.noreply.github.com>
2022-07-16 19:26:38 -0500

    AND gate

---
+++ and_gate.vhdl
@@ -0,0 +1,19 @@
+--Creation of an AND gate in VHDL
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity example_and is
+    port (
+      input_1   :  in std_logic;
+      input_2   :  in std_logic;
+      and_result : in std_logic
+      );
+  end example_and;
+  
+  architecture rtl of example_and is
+    signal and_gate : std_logic;
+  begin
+    and_gate  <=  input_1 and input_2;
+    and_result <= and_gate;
+  end rtl;
